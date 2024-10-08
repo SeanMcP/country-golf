364d23a601c010de004baa5f1727d27c199f162d
Sean McPherson <mail@seanmcp.com>
2024-10-07 21:11:58 -0400

    use better tile layer map
    Fixes #1

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ import * as shared from "./shared.js";
 const ZOOM = 7;
 let coordinates = [51.505, -0.09];
 let headerEl = document.querySelector("header");
-let formEl = document.querySelector('form')
+let formEl = document.querySelector("form");
 let strokes = [];
 
 // Initialize map
@@ -13,11 +13,7 @@ let map = L.map("map", {
   scrollWheelZoom: window.__devMode ?? false,
   zoomControl: window.__devMode ?? false,
 }).setView(coordinates, ZOOM);
-L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
-  maxZoom: 19,
-  attribution:
-    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-}).addTo(map);
+shared.addTileLayer(map);
 L.marker(coordinates).addTo(map).bindPopup("<b>Tee</b>").openPopup();
 
 // Select the target country
@@ -38,102 +34,113 @@ L.marker([center.geometry.coordinates[1], center.geometry.coordinates[0]])
   .addTo(map)
   .bindPopup(`<b>${DATA.properties.name}</b>`);
 
-formEl.addEventListener("submit", e => {
-    e.preventDefault();
-    let fd = new FormData(e.target)
-    let distance = parseFloat(fd.get("distance"))
-    let direction = fd.get("direction")
+formEl.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let fd = new FormData(e.target);
+  let distance = parseFloat(fd.get("distance"));
+  let direction = fd.get("direction");
 
-    switch (direction) {
-        case "N": {
-            coordinates[0] += distance / shared.MILES_PER_LATITUDE
-            break;
-        }
-        case "NE": {
-            let leg = getLegLength(distance)
-            coordinates[0] += leg / shared.MILES_PER_LATITUDE
-            coordinates[1] += leg / shared.MILES_PER_LONGITUDE
-            break;
-        }
-        case "E": {
-            coordinates[1] += distance / shared.MILES_PER_LONGITUDE
-            break;
-        }
-        case "SE": {
-            let leg = getLegLength(distance)
-            coordinates[0] -= leg / shared.MILES_PER_LATITUDE
-            coordinates[1] += leg / shared.MILES_PER_LONGITUDE
-            break;
-        }
-        case "S": {
-            coordinates[0] -= distance / shared.MILES_PER_LATITUDE
-            break;
-        }
-        case "SW": {
-            let leg = getLegLength(distance)
-            coordinates[0] -= leg / shared.MILES_PER_LATITUDE
-            coordinates[1] -= leg / shared.MILES_PER_LONGITUDE
-            break;
-        }
-        case "W": {
-            coordinates[1] -= distance / shared.MILES_PER_LONGITUDE
-            break;
-        }
-        case "NW": {
-            let leg = getLegLength(distance)
-            coordinates[0] += leg / shared.MILES_PER_LATITUDE
-            coordinates[1] -= leg / shared.MILES_PER_LONGITUDE
-            break;
-        }
+  switch (direction) {
+    case "N": {
+      coordinates[0] += distance / shared.MILES_PER_LATITUDE;
+      break;
     }
-
-    strokes.push(Array.from(coordinates))
-
-    console.log(coordinates)
-    map.flyTo(coordinates, ZOOM)
-    L.marker(coordinates).addTo(map).bindPopup(`<b>Stroke ${strokes.length}</b>`).openPopup()
-
-    let tPoint = turf.point([coordinates[1], coordinates[0]]);
-    console.log(">>> point", tPoint)
-    console.log(">>> distance (w/conversation)", turf.distance(tPoint, tCenter, { units: "miles" }))
-
-    // TODO: Clean this up
-    let isInside = false
-    try {
-        let polygon = turf.polygon(DATA.geometry.coordinates)
-        isInside = turf.booleanPointInPolygon(tPoint, polygon)
-    } catch {
-        isInside = DATA.geometry.coordinates.some(coords => {
-            let polygon = turf.polygon(coords);
-            console.log(">>>", polygon)
-            return turf.booleanPointInPolygon(tPoint, polygon);
-        })
+    case "NE": {
+      let leg = getLegLength(distance);
+      coordinates[0] += leg / shared.MILES_PER_LATITUDE;
+      coordinates[1] += leg / shared.MILES_PER_LONGITUDE;
+      break;
     }
-    // END TODO
-
-    console.log(">>> isInside", isInside)
-    printMessage();
-    if (isInside) {
-        headerEl.textContent = `You won in ${strokes.length} strokes!`
+    case "E": {
+      coordinates[1] += distance / shared.MILES_PER_LONGITUDE;
+      break;
     }
-})
+    case "SE": {
+      let leg = getLegLength(distance);
+      coordinates[0] -= leg / shared.MILES_PER_LATITUDE;
+      coordinates[1] += leg / shared.MILES_PER_LONGITUDE;
+      break;
+    }
+    case "S": {
+      coordinates[0] -= distance / shared.MILES_PER_LATITUDE;
+      break;
+    }
+    case "SW": {
+      let leg = getLegLength(distance);
+      coordinates[0] -= leg / shared.MILES_PER_LATITUDE;
+      coordinates[1] -= leg / shared.MILES_PER_LONGITUDE;
+      break;
+    }
+    case "W": {
+      coordinates[1] -= distance / shared.MILES_PER_LONGITUDE;
+      break;
+    }
+    case "NW": {
+      let leg = getLegLength(distance);
+      coordinates[0] += leg / shared.MILES_PER_LATITUDE;
+      coordinates[1] -= leg / shared.MILES_PER_LONGITUDE;
+      break;
+    }
+  }
+
+  strokes.push(Array.from(coordinates));
+
+  console.log(coordinates);
+  map.flyTo(coordinates, ZOOM);
+  L.marker(coordinates)
+    .addTo(map)
+    .bindPopup(`<b>Stroke ${strokes.length}</b>`)
+    .openPopup();
+
+  let tPoint = turf.point([coordinates[1], coordinates[0]]);
+  console.log(">>> point", tPoint);
+  console.log(
+    ">>> distance (w/conversation)",
+    turf.distance(tPoint, tCenter, { units: "miles" })
+  );
+
+  // TODO: Clean this up
+  let isInside = false;
+  try {
+    let polygon = turf.polygon(DATA.geometry.coordinates);
+    isInside = turf.booleanPointInPolygon(tPoint, polygon);
+  } catch {
+    isInside = DATA.geometry.coordinates.some((coords) => {
+      let polygon = turf.polygon(coords);
+      console.log(">>>", polygon);
+      return turf.booleanPointInPolygon(tPoint, polygon);
+    });
+  }
+  // END TODO
+
+  console.log(">>> isInside", isInside);
+  printMessage();
+  if (isInside) {
+    headerEl.textContent = `You won in ${strokes.length} strokes!`;
+  }
+});
 
 function getLegLength(hypotenuse) {
-    // a^2 + b^2 = c^2; a = b
-    // a^2 + a^2 = c^2
-    // 2a^2 = c^2
-    // a^2 = (c^2)/2
-    // a = sqrt((c^2)/2)
-    return Math.sqrt(Math.pow(hypotenuse, 2) / 2)
+  // a^2 + b^2 = c^2; a = b
+  // a^2 + a^2 = c^2
+  // 2a^2 = c^2
+  // a^2 = (c^2)/2
+  // a = sqrt((c^2)/2)
+  return Math.sqrt(Math.pow(hypotenuse, 2) / 2);
 }
 
-let totalDistance = turf.distance(tStart, tCenter, { units: "miles" })
+let totalDistance = turf.distance(tStart, tCenter, { units: "miles" });
 function printMessage() {
-    let message = ""
-    for (let i = 0; i < strokes.length; i++) {
-        let stroke = strokes[i]
-        let distance = turf.distance(turf.point([stroke[1], stroke[0]]), tCenter, { units: "miles" })
-        message += `Stroke ${i + 1}: ${((totalDistance - distance) / totalDistance * 100).toFixed(2)}%\n`
-    }
-    console.log(message)
+  let message = "";
+  for (let i = 0; i < strokes.length; i++) {
+    let stroke = strokes[i];
+    let distance = turf.distance(turf.point([stroke[1], stroke[0]]), tCenter, {
+      units: "miles",
+    });
+    message += `Stroke ${i + 1}: ${(
+      ((totalDistance - distance) / totalDistance) *
+      100
+    ).toFixed(2)}%\n`;
+  }
+  console.log(message);
 }
--- shared.js
+++ shared.js
@@ -6,3 +6,16 @@ export const HIGHLIGHTED_STYLE = {
 
 export const MILES_PER_LATITUDE = 69;
 export const MILES_PER_LONGITUDE = 54.6;
+
+export function addTileLayer(map) {
+  L.tileLayer(
+    "https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.{ext}",
+    {
+      minZoom: 0,
+      maxZoom: 20,
+      attribution:
+        '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      ext: "png",
+    }
+  ).addTo(map);
+}
